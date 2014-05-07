cd54565c229e9541c252459e29ba93ea5d13e9a0
Shlomi Matichin <shlomi@stratoscale.com>
2014-05-07 13:23:20 +0300

    PEP8 Style fixes

--- py/strato/whiteboxtest/infra/freeport.py
+++ py/strato/whiteboxtest/infra/freeport.py
@@ -1,10 +1,13 @@
 import socket
 
-def tcp(bindTo = "localhost"):
+
+def tcp(bindTo="localhost"):
     return _common(socket.SOCK_STREAM, bindTo)
 
-def udp(bindTo = "localhost"):
+
+def udp(bindTo="localhost"):
     return _common(socket.SOCK_DGRAM, bindTo)
+
 
 def _common(socketType, bindTo):
     sock = socket.socket(socket.AF_INET, socketType)
